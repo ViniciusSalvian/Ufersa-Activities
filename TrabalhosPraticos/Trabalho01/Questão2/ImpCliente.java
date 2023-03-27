6a96dd5d3960ff9e65863c2a6cfbc1129fc0d663
Inhegaz <vinipegacion@gmail.com>
2023-03-27 20:51:01 -0300

    New modifications

--- TrabalhosPraticos/Trabalho01/Questão2/ImpCliente.java
+++ TrabalhosPraticos/Trabalho01/Questão2/ImpCliente.java
@@ -9,6 +9,7 @@ public class ImpCliente implements Runnable {
   DatagramSocket cliente = null;
   int port;
   boolean msm;
+  String chave;
 
   public ImpCliente(int port, boolean msg) {
     this.msm = msg;
@@ -24,15 +25,9 @@ public class ImpCliente implements Runnable {
       Scanner teclado = new Scanner(System.in);
       Message msg = new Message();
       CifraVigenere cifraVigenere;
+      CifraVernam cifraVernam;
       String cypherText = "";
       String decryptedText = "";
-      String finalMessage = "";
-
-      System.out.println("Cifra de Vernam: 0; Cifra de Vigenere: 1; Cifra AES: 2");
-      System.out.println("Selecione o tipo de criptografia a ser aplicada: ");
-      msg.typeCriptography = teclado.nextInt();
-      System.out.println("Digite a chave: ");
-      msg.chave = teclado.next();
 
       if (msm) {
         System.out.print("Digite uma mensagem: ");
@@ -45,18 +40,35 @@ public class ImpCliente implements Runnable {
 
         if (msg.type == 0) {
           System.out.print("Digite o ID do destinatário obs: 1 até 3 -> ");
-          msg.id = teclado.nextInt();         
+          msg.id = teclado.nextInt();
         }
 
-        finalMessage = msg.mensagem + "-" + msg.type + "-" + msg.id + '-' + msg.typeCriptography + '-' + msg.chave;
+        System.out.println("Cifra de Vernam: 0; Cifra de Vigenere: 1; Cifra AES: 2");
+        System.out.println("Selecione o tipo de criptografia a ser aplicada: ");
+        msg.typeCriptography = teclado.nextInt();
+        
 
-        if (msg.typeCriptography == 1) {         
+        if (msg.typeCriptography == 0) {
+          cifraVernam = new CifraVernam();
+          msg.chave = cifraVernam.generateKey(msg.mensagem.length());
+          cypherText = cifraVernam.encrypt(msg.mensagem, msg.chave);
+          System.out.println("Mensagem cifrada: " + cypherText);
+          decryptedText = cifraVernam.decrypt(cypherText, msg.chave);
+          System.out.println("Mensagem decifrada: " + decryptedText);
+        }
+
+        if (msg.typeCriptography == 1) {
+          System.out.println("Digite a chave: ");
+          this.chave = teclado.next();
+          msg.chave = this.chave;
           cifraVigenere = new CifraVigenere(msg.chave);
-          cypherText = cifraVigenere.encrypt(finalMessage);
+          cypherText = cifraVigenere.encrypt(msg.mensagem);
           System.out.println("Mensagem cifrada: " + cypherText);
           decryptedText = cifraVigenere.decrypt(cypherText);
           System.out.println("Mensagem decifrada: " + decryptedText);
         }
+
+        cypherText = cypherText.concat("-" + msg.type + "-" + msg.id + '-' + msg.typeCriptography + '-' + msg.chave);
 
         System.out.println("Enviando mensagem para o servidor...");
 
@@ -77,8 +89,8 @@ public class ImpCliente implements Runnable {
         cliente.receive(datagramaRecebimento);
         bufferRecebimento = datagramaRecebimento.getData();
 
-        System.out.println("Mensagem recebida do servidor: " + new String(bufferRecebimento) +
-            "\n" + "Mensagem decriptografada: " + decryptedText);
+        System.out.println("Mensagem recebida do servidor: " + new String(bufferRecebimento));        
+
       } else {
 
         byte[] bufferRecebimento = new byte[2048];
@@ -89,17 +101,20 @@ public class ImpCliente implements Runnable {
         bufferRecebimento = datagramaRecebimento.getData();
         String msgRecebida = new String(bufferRecebimento);
         String [] msgSplit = msgRecebida.split("-"); 
-        String newMessage = msgSplit[3];
-        System.out.println( "Nova mensagem: " + newMessage);
+        String typeCriptography = msgSplit[3];
+      
 
-        int tipoCriptografia = Integer.parseInt(newMessage);
+        int tipoCriptografia = Integer.parseInt(typeCriptography);
+        String chave = msgSplit[4];
+
+        if (tipoCriptografia == 0) {
+          cifraVernam = new CifraVernam();
+          decryptedText = cifraVernam.decrypt(msgRecebida, chave);
+        }
 
         if (tipoCriptografia == 1) {
-          cifraVigenere = new CifraVigenere(msg.chave);
-          System.out.println("Chave: " + msg.chave);
-          System.out.println("testando " + decryptedText);
+          cifraVigenere = new CifraVigenere(chave);
           decryptedText = cifraVigenere.decrypt(msgRecebida);
-          System.out.println("Mensagem decriptografada do cao: " + decryptedText);
         }
 
         System.out.println("Mensagem recebida do servidor: " + new String(bufferRecebimento) +
